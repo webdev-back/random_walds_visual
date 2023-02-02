a57e9bd6c6f8ad2a746ca26761b11e623b5601ce
John Smith <smith@mail.com>
2023-02-02 16:14:57 +0300

    Test random walks with large number of steps

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active.
 while True:
     # Make a random walk.
-    rw = RandomWalk()
+    rw = RandomWalk(500_000)
     rw.fill_walk()
 
     # Plot the points in the walk.
     plt.style.use('classic')
-    fg, ax = plt.subplots()
+    fg, ax = plt.subplots(figsize=(10, 6))
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
 
     # Emphasize the first and last points.
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
 
+    # Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    plt.savefig('large_random_walk.png', bbox_inches='tight')
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
